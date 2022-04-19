a4fcddbb3e9efc9044a2636c594198b6462e157e
git config --global user.name Njugi <nmsteve45@gmail.com>
2022-04-19 13:47:13 +0300

    As obtained

---
+++ migrations/2_deploy_Assist.js
@@ -0,0 +1,7 @@
+// migrations/2_deploy_Assist.js
+const Assist = artifacts.require('Assist');
+
+module.exports =  function (deployer) {
+  deployer.deploy(Assist,'0xb05cB2b95e40A88661646AE64fa4C2B0B6233611','0xb05cB2b95e40A88661646AE64fa4C2B0B6233611','0xb05cB2b95e40A88661646AE64fa4C2B0B6233611','0xb05cB2b95e40A88661646AE64fa4C2B0B6233611','0xb05cB2b95e40A88661646AE64fa4C2B0B6233611');
+};
+
